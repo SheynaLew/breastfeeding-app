c360d893ec19ec8d09ef3886b38e1bb54d28ab84
Jenny <jenny.m.lewis@hotmail.co.uk>
2022-02-03 16:02:08 +0000

    font styling and colour added to the banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -6,7 +6,9 @@ import Title from '../Titles/index.js';
 
 //Create a Banner function and export as a default
 export default function Banner() {
+    //return a div which will be the banner and create the title text
     return <div className="banner">
-       <Title text = "Breast Buds"/>
+    {/* add the Title component with text props of "BreastBuds" */}
+       <Title text = "BreastBuds"/>
     </div>
 }
--- src/components/Titles/index.js
+++ src/components/Titles/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+//export a default function called Title which takes in a text prop
 export default function Title ({text}) {
+// return an h1 which includes the text prop
 return <h1>{text}</h1>
 }
