bde74bf25dc4a01343d2d55a7487954e7142ce1b
Jenny <jenny.m.lewis@hotmail.co.uk>
2022-02-03 14:41:05 +0000

    Title component set up

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Banner.css'
 
+import Title from '../Titles';
+
 
 //Create a Banner function and export as a default
 export default function Banner({title}) {
     return (
     <div className="banner">
-        {title} 
+        <h1>{title}</h1>
     </div>)
 }
