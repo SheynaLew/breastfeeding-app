790f0c2e55513839d38312d1bde95176024d0b7c
Jenny <jenny.m.lewis@hotmail.co.uk>
2022-02-06 22:45:12 +0000

    got calendar styled right and logging date value. Half finished the calculation from the button click

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,43 +1,45 @@
 import React, {useState} from 'react';
 
+import Calendar from "react-calendar";
+
 import '../App/App.css';
-// import 'react-calendar/dist/Calendar.css';
+import 'react-calendar/dist/Calendar.css';
 
 import Banner from '../Banner';
 import Button from '../Button';
-import Calendars from "../Calenders";
+// import Calendars from "../Calenders";
 import Title from '../Titles';
 
 // handleClick={submit}
 
 
 function App() {
+  //calendar useState
+  const [value, onChange] = useState(new Date());
+  const [lengthOfTime, setLengthOfTime] = useState();
+
+  function lengthOfTimeFeeding() {
+    setLengthOfTime(value[1] - value[0]);
+    //the above won't work because the date isn't an integer! need to turn date into integer first
+    const timeFeeding = lengthOfTime;
+    console.log(timeFeeding)
+  }
 
   return (
-  //   //useStates
-  //   const [startDate, setStartDate] = useState()
-  //   const [endDate, setEndDate] = useState()
-
-  //   //submit function for button click
-  //   function submit (){
-  //   setStartDate() 
-  //   }
-
     <div className="App">
       <Banner/>
       <div className="calendarsContainer">
         <div>
-          <Title text="Start Date" className="startDate"/>
-          <Calendars className="calendars"/>
-        </div>
-        <div>
-          <Title text="End Date" className="endDate"/>
-          <Calendars className="calendars"/>
+          <Title text="Journey Dates" className="startDate"/>
+          <div className="calendarCard">
+            <Calendar onChange={onChange} value={value} selectRange={true} className="calendar"/>
+          </div>
         </div>
       </div>
-      <Button  text={"submit"}/>
+      <Button handleClick={lengthOfTimeFeeding} text={"submit"}/>
     </div>
   ) 
 }
+
 
 export default App;
--- src/components/Calenders/index.js
+++ src/components/Calenders/index.js
@@ -1,16 +1,22 @@
-import React, {useState} from 'react';
-import Calendar from "react-calendar";
+// import React, {useState, useRef} from 'react';
+// import Calendar from "react-calendar";
 
-import 'react-calendar/dist/Calendar.css';
-import "./Calenders.css"
+// import 'react-calendar/dist/Calendar.css';
+// import "./Calenders.css"
 
-export default function Calendars() {
-    const [value, onChange] = useState(new Date());
+// export default function Calendars() {
+//     // const [value, onChange] = useState(new Date());
+//   // console.log(value)
 
-    return (
-      <div className="calendarCard">
-        <Calendar onChange={onChange} value={value} className="calendar"/>
-        {console.log(value)}
-      </div>
-    );
-}
+//     return (
+//       // <div className="calendarCard">
+//       //   <Calendar onChange={onChange} value={value} selectRange={Range} returnValue={Range} className="calendar"/>
+//       //   {console.log(value)}
+//       // </div>
+//     );
+// }
+
+
+// struggling to save calendar input on the button click. Had a look at useRef hook. Considering putting calendars and button into one component, then on the button click the calendars will move and the button will be hidden. Actually this won't work because it will create two buttons with the calendars. Need to find another way!
+  //As per Arshi's suggestion, using one calendar and selectRange.
+  //returnValue should allow dates to be passed by the calendar to the onChange function. Range will cause an array with start and end values to be passed.
