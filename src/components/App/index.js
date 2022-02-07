f63f2b5a0455c5eaeab103aabb67f131788902c8
Jenny <jenny.m.lewis@hotmail.co.uk>
2022-02-07 21:41:18 +0000

    submit button can now calculate the correct number of months spent breastfeeding based on calendar input

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -16,14 +16,37 @@ import Title from '../Titles';
 function App() {
   //calendar useState
   const [value, onChange] = useState(new Date());
-  const [lengthOfTime, setLengthOfTime] = useState();
+  // const [startTime, setStartTime] = useState(value);
+  // const [endTime, setEndTime] = useState(value);
+  // const [dateRange, setDateRange]  = useState(0);
 
-  function lengthOfTimeFeeding() {
-    setLengthOfTime(value[1] - value[0]);
-    //the above won't work because the date isn't an integer! need to turn date into integer first
-    const timeFeeding = lengthOfTime;
-    console.log(timeFeeding)
+  function monthDiff() {
+  let range = (value[1].getFullYear() - value[0].getFullYear()) * 12;
+  range -= value[0].getMonth();
+  range += value[1].getMonth();
+  //console.log("total months plus end month", range)
+  let startDay = value[0].getDate();
+  // console.log(startDay);
+  let endDay = value[1].getDate();
+  // console.log(endDay);
+  //need to include an if else statement so that the date is included and the number of months returned is not rounded up. Eg if someone started feeding on the 7th of the month, but they put their end date as the 6th of the month a year later, they are given 11 months, not 12.
+    //get the day of the month from each index of the value array and save to a variable
+    //if end day is less than start day, return range -1
+    //if end day is equal to or more than start day, return range
+    // console.log("range prior to checking statement", range)
+    if (endDay < startDay) {
+      console.log("endDay is less than startDay", range -1)
+      return range -1;
+    } else {
+      console.log("endDay is greater than or equal to startDay", range)
+      return range
+    }
   }
+  function getBreastfeedingAward() {
+    //A function which takes the month difference (monthdiff function) and returns a breastfeeding award and text.
+    monthDiff()
+  }
+  
 
   return (
     <div className="App">
@@ -36,10 +59,11 @@ function App() {
           </div>
         </div>
       </div>
-      <Button handleClick={lengthOfTimeFeeding} text={"submit"}/>
+      <Button handleClick={getBreastfeedingAward} text={"submit"}/>
     </div>
   ) 
 }
+
 
 
 export default App;
